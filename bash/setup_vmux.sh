c7dd5d0b820cd5e83f5410a78fd189f498d6fa3d
ManuelCanavarros <manuel.canavarros@gmail.com>
2020-11-30 15:36:34 -0500

    (fix) Fix VMUX python path execution by adding .py

--- bash/setup_vmux.sh
+++ bash/setup_vmux.sh
@@ -11,8 +11,10 @@ export VMUX_EDITOR
 # specific stuff to vmux session
 export EDITOR=vmux-editor
 export PATH="$PATH:$(dirname "$BASH_SOURCE")/commands"
-cd() {  
-  builtin cd "$@";
-  vmux_send :cd "$PWD"
-  echo "$PWD" > ~/.cwd;
+cd() {
+	. "$VMUXCLI_WORKDIR/config.shlib"
+	read_config
+	builtin cd "$@"
+	eval $VMUX_PYTHON_PATH $VMUX_PATH/commands/vmux_send.py :cd $PWD
+	echo "$PWD" >~/.cwd
 }
